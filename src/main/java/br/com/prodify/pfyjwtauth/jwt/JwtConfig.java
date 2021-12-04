36bb02a7f2f48564d4ba8b974c7863847ac87f8d
c-santana-sp <carlos.santana@bandtec.com.br>
2021-12-04 00:22:21 -0300

    First commit

---
+++ src/main/java/br/com/prodify/pfyjwtauth/jwt/JwtConfig.java
@@ -0,0 +1,42 @@
+package com.example.pfyjwtauth.jwt;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import com.google.common.net.HttpHeaders;
+
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix = "application.jwt")
+public class JwtConfig {
+
+    private String secretKey;
+    private String tokenPrefix;
+    private Integer tokenExpirationAfterDays;
+
+    public String getAuthorizationHeader() {
+        return HttpHeaders.AUTHORIZATION;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public String getTokenPrefix() {
+        return tokenPrefix;
+    }
+
+    public void setTokenPrefix(String tokenPrefix) {
+        this.tokenPrefix = tokenPrefix;
+    }
+
+    public Integer getTokenExpirationAfterDays() {
+        return tokenExpirationAfterDays;
+    }
+
+    public void setTokenExpirationAfterDays(Integer tokenExpirationAfterDays) {
+        this.tokenExpirationAfterDays = tokenExpirationAfterDays;
+    }
+}
---
+++ src/test/java/com/example/pfyjwtauth/PfyJwtAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pfyjwtauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfyJwtAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
